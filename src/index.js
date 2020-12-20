77e23f811e3618f83f28dfdae2fe5ec200dcbddc
hamub <hbutau35@gmail.com>
2020-12-20 10:27:32 +0200

    loading images

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import './style.css';
+import Picture from './hamub.jpg';
 
 function component() {
   const element = document.createElement('div');
@@ -7,6 +8,12 @@ function component() {
     //lodash now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello')
+
+    //add th e image to our existing div
+    const myImage = new Image();
+    myImage.src = Picture;
+
+    element.appendChild(myImage);
 
   return element;
 }
