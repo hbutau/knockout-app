ed74ecd6752b6897edaa14fa458d29581a4da977
hamub <hbutau35@gmail.com>
2020-12-21 23:01:38 +0200

    added webpack

--- src/index.js
+++ src/index.js
@@ -1,21 +1,86 @@
 import _ from 'lodash';
-import './style.css';
-import Picture from './hamub.jpg';
+// import materialize from 'materialize-css';
+import '/node_modules/materialize-css/dist/css/materialize.css';
+import '/src/styles/app.css';
+import '/src/styles/materialfonts.css';
+import ko from 'knockout';
+import $ from 'jquery';
 
-function component() {
-  const element = document.createElement('div');
+//diclare ViewModel.
 
-    //lodash now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello')
+function ProductsViewModel() {
+  var self = this;
 
-    //add th e image to our existing div
-    const myImage = new Image();
-    myImage.src = Picture;
+  self.getProducts = function() {
+    self.url = 'http://localhost:8000/api/products'
 
-    element.appendChild(myImage);
+    fetch('http://localhost:8000/api/products')
+      .then(function(res) {
+        return res.json();
+      })
+      .then(function(data) {
+        self.products = ko.observableArray(data);
 
-  return element;
+        self.name = ko.observable("");
+        self.sku = ko.observable("");
+        self.category_id = ko.observable("");
+        self.unit_price = ko.observable("");
+
+        self.addProduct = function() {
+          let data = {
+            name: self.name(),
+            sku: self.sku(),
+            category_id: self.category_id(),
+            unit_price: self.unit_price()
+
+          };
+
+          self.products.push(data)
+          console.log(data)
+          fetch(self.url, {
+              method: 'POST',
+              body: JSON.stringify(data),
+              headers: {
+                "Content-type": "application/json; charset=UTF-8"
+              }
+            })
+            .then(function(res) {
+              console.log(res);
+            })
+            .catch(function(err) {
+              console.err;
+            })
+
+          self.name('');
+          self.sku('');
+          self.category_id('');
+          self.unit_price('');
+        }
+        self.removeProduct = function(product) {
+          self.products.remove(product);
+          let product_id = {
+            product_id: product.id
+          }
+          fetch(
+            self.url + '/' + product.id, {
+              method: 'DELETE',
+              headers: {
+                "Content-type": "application/json; charset=UTF-8"
+              }
+            }
+          );
+          console.log(product.id)
+        }
+        ko.applyBindings(self);
+      });
+  }
 }
 
-document.body.appendChild(component());
+// $(document).ready(function() {
+
+//   let pros = new ProductsViewModel();
+//   pros.getProducts();
+// });
+
+let pros = new ProductsViewModel();
+pros.getProducts();
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,38 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 
 module.exports = {
-  entry: './src/index.js',
+    mode: 'development',
+  entry: {
+      index: './src/index.js',
+  },
+    plugins: [
+    new MiniCssExtractPlugin({
+        filename: 'bundle.css'
+    }),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+        title: 'Development',
+        template: 'src/index.html'
+    }),
+  ],
+    devtool: 'inline-source-map',
+    devServer: {
+    contentBase: './dist',
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
     module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-        {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
     ],
-  },
+    },
 };
